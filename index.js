b59413d1fd09a267faff0ddfd8f3ffb61508072f
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-05-14 14:31:49 -0700

    updated mongoose connection

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
      useNewUrlParser: true ,
-     useUnifiedTopology: true
+     useUnifiedTopology: true,
+     useCreateIndex: true,
+     useFindAndModify: false
     });
 
 // get all workouts
