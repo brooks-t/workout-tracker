f094aef58775928116129f47c33286d0d72c3e97
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-05-14 13:31:56 -0700

    updated routes, models, and README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# workout-tracker
+# FITNESS TRACKER
+
+## DESCRIPTION
+The fitness tracker allows you to track your workouts by adding resistance or cardio exercises to a new workout or update an existing. You can also see your fitness stats on the workout dashboard.
+
+## INSTRUCTIONS
+**Adding a new workout**
+- Click on the "new workout" button to add a new work out. - Then use the exercise type dropdown menu to select the type of exercise you'd like to add to your work out (resistance or cardio)
+- Fill out the fields for resistance or cardio then click "add exercise" to add it to your work out.
+- Click "complete" once you've finished adding all the exercises you need to add to your work out.
+
+**Updating your last workout**
+- Click on "continue workout"
+- Update your workout by adding an exercise with the "add exercise" button just like the instructions above.
+- When you are done, click the "complete" button to save the workout.
+
+**Viewing your stats**
+- Click on the "Dashboard" link to the top left to view your workout dashboard.
+
+## LINK
+
+## SCREENSHOT
+![screenshot of fitness tracker](public/img/screenshot.png)
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
@@ -11,15 +12,46 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
      useNewUrlParser: true ,
      useUnifiedTopology: true
     });
 
+// get all workouts
 app.get("/api/workouts",(req,res)=>{
     db.Workout.find().then(allWorkouts=>{
-        res.json(allWorkouts)
+        res.json(allWorkouts);
     }).catch(err=>{
         res.status(500).json(err);
     })
+})
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+  });
+
+app.get("/stats", (req, res) => {
+res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+// add an exercise
+app.put('/api/workouts/:workoutid',(req,res)=>{
+    db.Workout.findOneAndUpdate({_id:req.params.workoutid}, {$push: {exercises: req.body}}, {new:true}).then(newExercise=>{
+        res.json(newExercise)
+    }).catch(err=>{
+        res.status(500).json(err);
+    })
+})
+
+// create a workout
+app.post('/api/workouts',(req,res)=>{
+    db.Workout.create(req.body).then(newWorkout=>{
+        res.json(newWorkout)
+    }).catch(err=>{
+        res.status(500).json(err);
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("listenin on port " + PORT)
 })
